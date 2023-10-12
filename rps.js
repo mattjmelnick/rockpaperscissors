da8373a1cc37e1f9495f6e5df17235dd134ecbd3
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2023-10-12 00:36:03 -0400

    Add getPlayerSelection function, prompts user for rps, normalizes the input, and recursively prompts again until rps is chosen

--- rps.js
+++ rps.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+function getPlayerSelection() {
+    let playerChoice = prompt("Choose rock, paper, or scissors:");
+    let playerSelection = playerChoice.toLowerCase();
+    
+    /*if (playerSelection == "rock" ||
+        playerSelection == "paper" ||
+        playerSelection == "scissors") {
+            return playerSelection;
+    }
+    else {
+        let chooseAgain = getPlayerSelection()
+        return chooseAgain;
+    }
+}
+*/
+
+   switch(playerSelection) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return playerSelection;
+            break;
+        default:
+            let chooseAgain = getPlayerSelection();
+            return chooseAgain;
+
+}
+}
+
+console.log(getPlayerSelection());
