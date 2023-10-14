9093587d9c532590045b6aae2ac011f9b9efc037
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2023-10-13 23:46:21 -0400

    Add playRound function with correct win/lose/tie conditional logic

--- rps.js
+++ rps.js
@@ -1,37 +1,61 @@
 function getPlayerSelection() {
     let playerChoice = prompt("Choose rock, paper, or scissors:");
-    let playerSelection = playerChoice.toLowerCase();
+    let playerRPS = playerChoice.toLowerCase();
     
-    if (playerSelection == "rock" ||
-        playerSelection == "paper" ||
-        playerSelection == "scissors") {
-            return playerSelection;
+    if (playerRPS == "rock" ||
+        playerRPS == "paper" ||
+        playerRPS == "scissors") {
+            return playerRPS;
     }
     else {
         let chooseAgain = getPlayerSelection()
         return chooseAgain;
     }
 
-    // How the code works with a switch statement //
+     // How the code works with a switch statement //
     /*
-   switch(playerSelection) {
+   switch(playerRPS) {
         case "rock":
         case "paper":
         case "scissors":
-            return playerSelection;
+            return playerRPS;
             break;
         default:
             let chooseAgain = getPlayerSelection();
             return chooseAgain;
     }
     */
-}
+    }
 
 function getComputerSelection() {
     const RPS = ["rock", "paper", "scissors"];
-    let computerSelection = RPS[Math.floor(Math.random() * RPS.length)];
-    return computerSelection;
+    let computerRPS = RPS[Math.floor(Math.random() * RPS.length)];
+    return computerRPS;
 }
 
-console.log(getPlayerSelection());
-console.log(getComputerSelection());
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerSelection();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        let tie = console.log(`${playerSelection} and ${computerSelection} are the same - TIE`);
+        return tie;
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors" ||
+             playerSelection == "paper" && computerSelection == "rock" ||
+             playerSelection == "scissors" && computerSelection == "paper") {
+                let playerWin = console.log(`${playerSelection} beats ${computerSelection} - WIN`);
+                return playerWin;
+    }
+    else {
+        let playerLose = console.log(`${computerSelection} beats ${playerSelection} - LOSE`);
+        return playerLose;
+    }
+}
+
+let result = playRound(playerSelection, computerSelection);
+console.log(result);
